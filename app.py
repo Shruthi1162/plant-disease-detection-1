ae5a5c6bd1826f26c742305eac14d66d943d7aac
Shruthi1162 <24eg105g11@anurag.edu.in>
2026-02-07 10:46:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown("""
 # ---------------------------
 # Title
 # ---------------------------
-st.markdown("<div class='main-title'>🌱 Plan Leaf Disease Detection</div>", unsafe_allow_html=True)
+st.markdown("<div class='main-title'>🌱 Plant Leaf Disease Detection</div>", unsafe_allow_html=True)
 st.caption("Upload a tomato leaf image to detect disease.")
 
 # ---------------------------
